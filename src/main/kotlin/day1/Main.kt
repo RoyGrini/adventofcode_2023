b7e76386388ba49bcc4013a61d93d674b4f16449
Roy Grini <roy.grini@nav.no>
2023-12-04 09:01:59 +0100

    Day2

--- src/main/kotlin/day1/Main.kt
+++ src/main/kotlin/day1/Main.kt
@@ -9,48 +9,82 @@ fun main(args: Array<String>) {
     readInput(input)?.forEach { s ->
         val digits = s.filter { it.isDigit() }
         val num = "" + digits.first() + digits.last()
-        allCalibNums.add( num.toInt() )
+        allCalibNums.add(num.toInt())
 //        println("$s  ->  ${allCalibNums.last()}")
     }
     println("Sum unconverted: ${allCalibNums.sum()}")
 
+//    input = "day1/test2.txt"
     allCalibNums.clear()
     readInput(input)?.forEach { s ->
-        val converted = s.convertWordDigits()
-        val digits = converted.filter { it.isDigit() }
+        val digits = s.convertWordDigits()
+//        val digits = converted.filter { it.isDigit() }
         val num = "" + digits.first() + digits.last()
-        allCalibNums.add( num.toInt() )
+        allCalibNums.add(num.toInt())
         println("$s  ->  ${allCalibNums.last()}")
     }
     println("Sum converted: ${allCalibNums.sum()}")
+    // Test2 281 + 61 = 342
 }
 
 private fun String.convertWordDigits(): String {
 //    println(this)
     var result = ""
     var tmp = ""
-    this.forEach {
-        if (it.isLetter()) {
-            tmp += it
-            tmp = tmp
-                .replace("one", "1")
-                .replace("two", "2")
-                .replace("three", "3")
-                .replace("four", "4")
-                .replace("five", "5")
-                .replace("six", "6")
-                .replace("seven", "7")
-                .replace("eight", "8")
-                .replace("nine", "9")
-            if (tmp.last().isDigit()) {
-                result += tmp.last()
+    run breaking@{
+        this.forEach {
+            if (it.isLetter()) {
+                tmp += it
+                tmp = tmp
+                    .replace("one", "1")
+                    .replace("two", "2")
+                    .replace("three", "3")
+                    .replace("four", "4")
+                    .replace("five", "5")
+                    .replace("six", "6")
+                    .replace("seven", "7")
+                    .replace("eight", "8")
+                    .replace("nine", "9")
+                if (tmp.last().isDigit()) {
+                    result += tmp.last()
+                    tmp = ""
+                    return@breaking
+                }
+            } else {
+                result += it
                 tmp = ""
+                return@breaking
             }
-        } else {
-            result += it
-            tmp = ""
+//            println("$it - $tmp - $result")
         }
-//        println("$it - $tmp - $result")
+    }
+
+    run breaking@ {
+        this.reversed().forEach {
+            if (it.isLetter()) {
+                tmp = it + tmp
+                tmp = tmp
+                    .replace("one", "1")
+                    .replace("two", "2")
+                    .replace("three", "3")
+                    .replace("four", "4")
+                    .replace("five", "5")
+                    .replace("six", "6")
+                    .replace("seven", "7")
+                    .replace("eight", "8")
+                    .replace("nine", "9")
+                if (tmp.first().isDigit()) {
+                    result += tmp.first()
+                    tmp = ""
+                    return@breaking
+                }
+            } else {
+                result += it
+                tmp = ""
+                return@breaking
+            }
+//            println("$it - $tmp - $result")
+        }
     }
     return result
 }
---
+++ src/main/kotlin/day2/Main.kt
@@ -0,0 +1,47 @@
+package day1
+
+import readInput
+import kotlin.math.max
+
+fun main(args: Array<String>) {
+    var input = "day2/input.txt"
+
+    val testMax = mapOf("red" to 12, "blue" to 14, "green" to 13)
+    val games = mutableMapOf<Int, Map<String, Int>>()
+    val gamePower = mutableMapOf<Int, Int>()
+    val possibleGames = ArrayList<Int>()
+    readInput(input)?.forEach { s ->
+        val (gameNum, result) = parseLine(s)
+        if (possible(testMax, result)) {
+            possibleGames.add(gameNum)
+        }
+        gamePower[gameNum] = gamePower(result)
+    }
+    println("Possible games: $possibleGames")
+    println("Possible games sum:" + possibleGames.sum())
+    println("Game powers: $gamePower")
+    println("Game powers sum:" + gamePower.values.sum())
+}
+
+fun parseLine(line: String) : Pair<Int, Map<String, Int>> {
+    val result = mutableMapOf<String, Int>()
+    val game = line.split(":")
+    val gameNum = game[0].split(" ")[1].toInt()
+    val draws = game[1].split(";")
+    for (draw in draws) {
+        val cubes = draw.split(",")
+        for (cube in cubes) {
+            val (count, color) = cube.trim().split(" ")
+            result[color] = max(result.getOrDefault(color, 0), count.toInt())
+        }
+    }
+    return Pair(gameNum, result)
+}
+
+fun possible(max: Map<String, Int>, game: Map<String, Int>) : Boolean {
+    return max.all { (color, count) -> game.getOrDefault(color, 0) <= count }
+}
+
+fun gamePower(game: Map<String, Int>) : Int {
+    return game.values.reduce{ acc, i -> acc * i }
+}
